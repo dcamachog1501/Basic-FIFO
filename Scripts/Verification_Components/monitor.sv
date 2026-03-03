1c1d2e222f79592055be4ee38a45bdfc7b5a7d3f
dcama <dcamachog1501@gmail.com>
2026-03-03 15:16:45 -0600

    Reformatting of Verification Component + Debugging (Monitor)

--- Scripts/Verification_Components/monitor.sv
+++ Scripts/Verification_Components/monitor.sv
@@ -3,69 +3,53 @@
 // Observes DUT interface signals, creates sequence items,
 // and broadcasts them via analysis port.
 //------------------------------------------------------------
-class FIFO_monitor extends uvm_monitor;
 
-    // Register component with UVM factory
-    `uvm_component_utils(FIFO_monitor)
+class FIFO_Monitor extends uvm_monitor;
 
-    // Analysis port to send observed transactions to subscribers
-    uvm_analysis_port #(FIFO_Seq_Item) mon_analysis_port;
+    // Factory registration
+    `uvm_component_utils(FIFO_Monitor)
 
-    // Virtual interface handle to connect to DUT signals
+    // Analysis port for observed transactions
+    uvm_analysis_port #(FIFO_Seq_Item#(FIFO_WIDTH)) mon_analysis_port;
+
+    // Virtual interface handle
     virtual FIFO_if fifo_if;
 
-    //--------------------------------------------------------
     // Constructor
-    //--------------------------------------------------------
-    function new(string name="FIFO_Monitor", uvm_component parent);
-        super.new(name,parent)
+    function new(string name = "monitor", uvm_component parent);
+        super.new(name, parent);
     endfunction
 
-    //--------------------------------------------------------
     // Build phase: retrieve interface and create analysis port
-    //--------------------------------------------------------
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
 
-        // Get virtual interface from config DB
-        if(!uvm_config_db#(virtual FIFO_if)::get(null,"","fifo_if",this.fifo_if))
-            `uvm_fatal("MONITOR","ABORTING -- Interface couldn't be retrieved!");
-        
-        // Create analysis port
-        this.mon_analysis_port = new("mon_analysis_port",this);
+        if (!uvm_config_db#(virtual FIFO_if)::get(null, "", "fifo_if", fifo_if))
+            `uvm_fatal("MONITOR", "Interface could not be retrieved");
+
+        mon_analysis_port = new("mon_analysis_port", this);
     endfunction
 
-    //--------------------------------------------------------
-    // Run phase: continuously sample DUT signals
-    //--------------------------------------------------------
-    task void run_phase(uvm_phase phase);
+    // Run phase: sample DUT signals and broadcast transactions
+    task run_phase(uvm_phase phase);
         super.run_phase(phase);
 
-        forever
-        begin
-            // Synchronize to monitor clocking block
-            @(this.fifo_if.mon_mp.mon_cb)
+        forever begin
+            @(fifo_if.mon_mp.mon_cb);
 
-            // Only capture transaction when VALID_DRV is asserted
-            if(this.fifo_if.mon_mp.mon_cb.VALID_DRV)
-            begin
-                // Create new sequence item
-                FIFO_Seq_Item seq_item = FIFO_Seq_Item::type_id::create();
+            if (fifo_if.mon_mp.mon_cb.VALID_DRV) begin
+                FIFO_Seq_Item #(FIFO_WIDTH) seq_item =FIFO_Seq_Item#(FIFO_WIDTH)::type_id::create();
 
-                // Sample DUT signals into sequence item
-                seq_item.LOAD      = this.fifo_if.mon_mp.mon_cb.LOAD;
-                seq_item.POP       = this.fifo_if.mon_mp.mon_cb.POP;
-                seq_item.VALUE_IN  = this.fifo_if.mon_mp.mon_cb.VALUE_IN;
-                seq_item.EMPTY     = this.fifo_if.mon_mp.mon_cb.EMPTY;
-                seq_item.FULL      = this.fifo_if.mon_mp.mon_cb.FULL;
-                seq_item.VALUE_OUT = this.fifo_if.mon_mp.mon_cb.VALUE_OUT;
-                seq_item.VALID_DRV = this.fifo_if.mon_mp.mon_cb.VALID_DRV;
+                seq_item.LOAD      = fifo_if.mon_mp.mon_cb.LOAD;
+                seq_item.POP       = fifo_if.mon_mp.mon_cb.POP;
+                seq_item.VALUE_IN  = fifo_if.mon_mp.mon_cb.VALUE_IN;
+                seq_item.EMPTY     = fifo_if.mon_mp.mon_cb.EMPTY;
+                seq_item.FULL      = fifo_if.mon_mp.mon_cb.FULL;
+                seq_item.VALUE_OUT = fifo_if.mon_mp.mon_cb.VALUE_OUT;
 
-                // Print snooped transaction for debug
-                `uvm_info("MONITOR",$sformatf("Snooped pkg: %s",seq_item.convert2string()),UVM_LOW);
-
-                // Broadcast transaction to connected subscribers
-                this.mon_analysis_port.write(seq_item);
+                `uvm_info("MONITOR",$sformatf("Snooped transaction: %s",seq_item.convert2string()),UVM_LOW);
+                
+                mon_analysis_port.write(seq_item);
             end
         end
     endtask
