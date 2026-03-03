de7bf1c05d34a12b6cf2640c7be5b21d8e6ca406
dcama <dcamachog1501@gmail.com>
2026-03-03 15:08:09 -0600

    Reformatting of Verification Component + Debugging (Driver)

--- Scripts/Verification_Components/driver.sv
+++ Scripts/Verification_Components/driver.sv
@@ -3,107 +3,82 @@
 // Drives DUT interface signals based on sequence items
 // received from sequencer.
 //------------------------------------------------------------
-class FIFO_driver extends uvm_driver #(FIFO_Seq_Item);
 
-    // Virtual interface handle to connect to DUT signals
+class FIFO_Driver extends uvm_driver #(FIFO_Seq_Item#(FIFO_WIDTH));
+
+    // Virtual interface handle
     virtual FIFO_if fifo_if;
 
-    // Register component with UVM factory
-    `uvm_component_utils(FIFO_driver)
+    // Factory registration
+    `uvm_component_utils(FIFO_Driver)
 
-    //--------------------------------------------------------
     // Constructor
-    //--------------------------------------------------------
-    function new(string name="FIFO_Driver",uvm_component parent);
-        super.new(name,parent);
+    function new(string name = "driver", uvm_component parent);
+        super.new(name, parent);
     endfunction
 
-    //--------------------------------------------------------
     // Build phase: retrieve interface from config DB
-    //--------------------------------------------------------
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
 
-        // Get virtual interface from config DB
-        if(!uvm_config_db#(virtual FIFO_if)::get(null,"","fifo_if",this.fifo_if))
-            `uvm_fatal("DRIVER","ABORTING -- Interface couldn't be retrieved!");
+        if (!uvm_config_db#(virtual FIFO_if)::get(null, "", "fifo_if", fifo_if))
+            `uvm_fatal("DRIVER", "Interface could not be retrieved");
     endfunction
 
-    //--------------------------------------------------------
     // Run phase: main driver loop
-    //--------------------------------------------------------
-    task void run_phase(uvm_phase phase);
+    task run_phase(uvm_phase phase);
         super.run_phase(phase);
 
-        forever 
-        begin
-            // If reset is active, reset interface signals
-            if(this.fifo_if.RST)
-                this.reset_if();
-            else
-            begin
-                // Try to get next sequence item from sequencer
+        forever begin
+            if (fifo_if.RST)
+                reset_if();
+            else begin
                 FIFO_Seq_Item seq_item;
-                this.seq_item_port.try_next_item(seq_item);
+                seq_item_port.try_next_item(seq_item);
 
-                if(seq_item)
-                begin
-                    // Drive DUT interface with sequence item
-                    this.drive_if(seq_item);
-
-                    // Notify sequencer that item is done
-                    this.seq_item_port.item_done();
+                if (seq_item) begin
+                    drive_if(seq_item);
+                    seq_item_port.item_done();
                 end
                 else
-                    // No item available, release interface
-                    this.release_if();
+                    release_if();
             end
         end
     endtask
 
-    //--------------------------------------------------------
     // Reset interface signals
-    //--------------------------------------------------------
-    task void reset_if();
-        `uvm_info("DRIVER","Reseting Interface ...",UVM_LOW);
+    task reset_if();
+        `uvm_info("DRIVER", "Resetting interface", UVM_LOW);
 
-        this.fifo_if.drv_mp.LOAD      <= 0;
-        this.fifo_if.drv_mp.POP       <= 0;
-        this.fifo_if.drv_mp.VALUE_IN  <= 0;
-        this.fifo_if.drv_mp.VALID_DRV <= 0;
+        fifo_if.drv_mp.drv_cb.LOAD      <= 0;
+        fifo_if.drv_mp.drv_cb.POP       <= 0;
+        fifo_if.drv_mp.drv_cb.VALUE_IN  <= 0;
+        fifo_if.drv_mp.drv_cb.VALID_DRV <= 0;
     endtask
 
-    //--------------------------------------------------------
     // Drive interface with sequence item
-    //--------------------------------------------------------
-    task void drive_if(FIFO_Seq_Item seq_item);
-        // Synchronize to driver clocking block
-        @(this.fifo_if.drv_cb);
+    task drive_if(FIFO_Seq_Item seq_item);
+        @(fifo_if.drv_cb);
 
-        `uvm_info("DRIVER","Driving Interface ...",UVM_LOW);
-        `uvm_info("DRIVER",seq_item.convert2string(),UVM_LOW);
+        `uvm_info("DRIVER", "Driving interface", UVM_LOW);
+        `uvm_info("DRIVER", seq_item.convert2string(), UVM_LOW);
 
-        // Apply sequence item values to DUT interface
-        this.fifo_if.drv_mp.LOAD      <= seq_item.LOAD;
-        this.fifo_if.drv_mp.POP       <= seq_item.POP;
-        this.fifo_if.drv_mp.VALUE_IN  <= seq_item.VALUE_IN;
-        this.fifo_if.drv_mp.VALID_DRV <= 1;
+        fifo_if.drv_mp.drv_cb.LOAD      <= seq_item.LOAD;
+        fifo_if.drv_mp.drv_cb.POP       <= seq_item.POP;
+        fifo_if.drv_mp.drv_cb.VALUE_IN  <= seq_item.VALUE_IN;
+        fifo_if.drv_mp.drv_cb.VALID_DRV <= 1;
     endtask
 
-    //--------------------------------------------------------
     // Release interface (set signals to idle)
-    //--------------------------------------------------------
-    task void release_if ();
-        // Synchronize to driver clocking block
-        @(this.fifo_if.drv_cb);
+    task release_if();
+        @(fifo_if.drv_cb);
 
-        `uvm_info("DRIVER","Releasing Interface ...",UVM_LOW);
-        
-        // Clear signals
-        this.fifo_if.drv_mp.LOAD      <= 0;
-        this.fifo_if.drv_mp.POP       <= 0;
-        this.fifo_if.drv_mp.VALUE_IN  <= 0;
-        this.fifo_if.drv_mp.VALID_DRV <= 0;
+        `uvm_info("DRIVER", "Releasing interface", UVM_LOW);
+
+        fifo_if.drv_mp.drv_cb.LOAD      <= 0;
+        fifo_if.drv_mp.drv_cb.POP       <= 0;
+        fifo_if.drv_mp.drv_cb.VALUE_IN  <= 0;
+        fifo_if.drv_mp.drv_cb.VALID_DRV <= 0;
     endtask
 
 endclass
