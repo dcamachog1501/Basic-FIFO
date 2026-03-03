b29c3e00a6314efa2be50bdeb3bc378766edf89c
dcama <dcamachog1501@gmail.com>
2026-03-03 15:28:55 -0600

    Reformatting of Verification Component + Debugging (Sequence)

--- Scripts/Verification_Components/sequence.sv
+++ Scripts/Verification_Components/sequence.sv
@@ -1,34 +1,35 @@
-// UVM sequence class for generating FIFO transactions
-class FIFO_Sequence extends uvm_sequence #(FIFO_Seq_Item);
+//------------------------------------------------------------
+// FIFO Sequence
+// Generates a specified number of FIFO transactions.
+//------------------------------------------------------------
 
-    // Number of transactions to generate in this sequence
+class FIFO_Sequence extends uvm_sequence #(FIFO_Seq_Item#(FIFO_WIDTH));
+
+    // Number of transactions to generate
     int trans_amount;
 
-    // Register this sequence with the UVM factory
+    // Factory registration
     `uvm_object_utils(FIFO_Sequence)
 
-    // Constructor: allows specifying the number of transactions
-    function new (string name="FIFO_Sequence", int trans_amount=0);
+    // Constructor: set sequence name and transaction count
+    function new(string name = "sequence");
         super.new(name);
-        this.trans_amount = trans_amount;
     endfunction
 
-    // Main sequence behavior: generates 'trans_amount' transactions
+    // Sequence body: create and send 'trans_amount' items
     task body();
-
-        // Loop to create and send the specified number of items
         for (int i = 0; i < this.trans_amount; i++) begin
-            FIFO_Seq_Item seq_item = FIFO_Seq_Item::type_id::create(); // Creating the sequence_item to be sent (Using the Factory)
-            start_item(seq_item);                                      // Handshake with sequencer
-            seq_item.randomize();                                      // Randomize transaction fields
-            finish_item(seq_item);                                     // Send item to driver
+            FIFO_Seq_Item #(FIFO_WIDTH) seq_item =
+                FIFO_Seq_Item#(FIFO_WIDTH)::type_id::create(); // Create item via factory
+
+            start_item(seq_item);   // Handshake with sequencer
+            seq_item.randomize();   // Randomize transaction fields
+            finish_item(seq_item);  // Send item to driver
         end
 
-        // Informational message once sequence completes
         `uvm_info("SEQUENCE",
-                  $sformat("Finished Generating %0d Items", this.trans_amount),
+                  $sformatf("Finished generating %0d items", this.trans_amount),
                   UVM_LOW)
-
     endtask
 
 endclass
